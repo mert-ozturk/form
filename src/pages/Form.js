5d50e5b4e13e5faad0d8c241254dbf22687573f6
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-11-26 20:09:18 +0300

    islem2

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -5,13 +5,13 @@ import * as yup from 'yup'
 
 const Form = () => {
   const schema = yup.object().shape({
-    fullName: yup.string().required(),
+    fullName: yup.string().required("Your Full Name is Required!"),
     email: yup.string().email().required(),
     age: yup.number().positive().integer().min(18).required(),
     password: yup.string().min(4).max(20).required(),
     confirmPassword:yup.string().oneOf([yup.ref("password"),null]).required(),
   })
-  const {register,handleSubmit} = useForm({
+  const {register,handleSubmit,formState:{errors}} = useForm({
     resolver: yupResolver(schema)
   });
 
@@ -22,6 +22,7 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <input type="text" placeholder="Full Name" {...register("fullname")}/>
+        <p>{errors.fullName?.message}</p>
         <input type="text" placeholder="Email" {...register("email")}/>
         <input type="number" placeholder="Age" {...register("age")}/>
         <input type="password" placeholder="Password" {...register("password")}/>
